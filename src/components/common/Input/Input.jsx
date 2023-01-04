79f86e743b7b5bc7498604fd3a81c2ff44208585
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-01-04 15:08:36 +0330

    feat: hide and unHide password icon add

--- src/components/common/Input/Input.jsx
+++ src/components/common/Input/Input.jsx
@@ -1,4 +1,6 @@
 import styles from "./input.module.css";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { useState } from "react";
 
 const Input = ({
   label,
@@ -6,25 +8,43 @@ const Input = ({
   name,
   type = "text",
   onChange,
+  onBlur,
   value,
   error,
+  touched,
 }) => {
-  // console.log(formik)
+  const [showPassword, setShowPassword] = useState(true);
+  const iconClicked = () => {
+    showPassword ? setShowPassword(false) : setShowPassword(true);
+  };
+  const renderIcon = () => {
+    if (type === "password")
+      return showPassword ? (
+        <FaEyeSlash className={styles.customStyle} onClick={iconClicked} />
+      ) : (
+        <FaEye className={styles.customStyle} onClick={iconClicked} />
+      );
+  };
   return (
     <>
       <label htmlFor={name} className={styles.inputControl}>
-        <div>
+        <div className={styles.ErrorControl}>
           <span>{label}</span>
-          <span className={styles.errorText}>{error && error}</span>
+          <span className={styles.errorText}>{error && touched && error}</span>
         </div>
-        <input
-          id={name}
-          placeholder={placeholder}
-          type={type}
-          name={name}
-          onChange={onChange}
-          value={value}
-        />
+        <div className={styles.passwordControl}>
+          <input
+            id={name}
+            placeholder={placeholder}
+            type={showPassword ? "password" : "text"}
+            name={name}
+            onChange={onChange}
+            onBlur={onBlur}
+            value={value}
+            style={{ width: type === "password" ? "90%" : "100%" }}
+          />
+          {renderIcon()}
+        </div>
       </label>
     </>
   );
